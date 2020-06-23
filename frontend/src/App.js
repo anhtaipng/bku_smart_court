a8d4291cb2e86da347588b3d08449af81f92976e
Tien <tien.tm@pillez.tech>
2020-06-23 23:25:44 +0700

    sửa background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ import ProductScreen from './screens/ProductScreen';
 import { useSelector } from 'react-redux';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
-
 function App() {
 
 
@@ -44,8 +43,8 @@ function App() {
         <aside className="sidebar">
           <h3 className="item">Shopping Categories</h3>
           <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-              <a className="item item-btn" href="index.html">Food</a>
-              <a className="item item-btn" href="index.html">Drinks</a>
+              <a className="item item-btn" href="#">Food</a>
+              <a className="item item-btn" href="#">Drinks</a>
         </aside>
         <main className="main">
           <div className="content">
@@ -53,9 +52,8 @@ function App() {
             <Route path="/product/:id" component={ProductScreen} />
             <Route path="/signin" component={SigninScreen} />
             <Route exact path="/" component={HomeScreen} />
-
           </div>
-
+          <img className="background" src="/images/bg.jpg"></img>
         </main>
     <footer className="footer">
           4T1S
