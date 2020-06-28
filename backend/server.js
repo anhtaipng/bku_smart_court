c46ebd83137e01e10a7c38c06b9c62fe8e99b360
Tai Pham <anhtaipng@gmail.com>
2020-06-28 15:16:11 +0700

    done sign in and register

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mysql from 'mysql';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -14,7 +17,7 @@ const connection = mysql.createConnection({
     (err) ? console.log(err) : console.log(connection);
   });
 
-
+  
 //tai data cho home screen
 app.get("/api/products", (req, res) => {
     const sql = "SELECT * FROM products";
@@ -35,7 +38,52 @@ app.get("/api/products/:id", (req, res) => {
       });
 });
 
+//xu ly sign in
+app.post("/api/users/signin", (req, res) => {
+  const sql = "SELECT * FROM users";
+  connection.query(sql, function (err, results) {
+    if (err) throw err;
+    const signinUser = results.find(x => x.email === req.body.email);
+    if (signinUser) {
+      if (signinUser.password === req.body.password)
+        res.send({
+          _id: signinUser._id,
+          name: signinUser.name,
+          email: signinUser.email,
+          isAdmin: signinUser.isAdmin,
+        });
+      else res.status(401).send({ message: 'Password wrong.' });
+    }
+    else res.status(401).send({ message: 'Invalid Email or Password.' });
+  });
+});
 
+
+//xu ly register
+app.post("/api/users/register", (req, res) => {
+  var sql = "INSERT INTO users(name,email,password,isAdmin) VALUES(?,?,?,?)";
+  const prepare = [req.body.name,req.body.email,req.body.password,0];
+  sql = connection.format(sql,prepare);
+  connection.query(sql, function(err,results){
+    if (err) throw err;
+    console.log("Register thanh cong");
+  });
+
+  sql = "SELECT * FROM users";
+  connection.query(sql, function(err,results){
+    if (err) throw err;
+    const newUser= results.find(x => x.email === req.body.email);
+    if (newUser)
+    res.send({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+    else res.status(401).send({ msg: 'Invalid User Data.' });
+  })
+
+});
 
 app.listen(5000, () => {
     console.log("server at port 5000");
